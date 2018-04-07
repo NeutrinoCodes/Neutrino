51b3c01bc7564e7ee1ebbece8025591c17df6b93
Erik Zorzin <erik@zorzin.com>
2018-04-07 23:40:09 +0200

    Allocated points and colors on the heap: better for memory management.

--- Code/src/program.cpp
+++ Code/src/program.cpp
@@ -13,8 +13,8 @@
 #define DX              0.02f
 #define DY              0.02f
 
-data_float4 points(NUM_POINTS);
-data_float4 colors(NUM_POINTS);
+data_float4* points = new data_float4(NUM_POINTS);
+data_float4* colors = new data_float4(NUM_POINTS);
 float tick;
 
 void load()
@@ -26,7 +26,7 @@ void load()
 
 void setup()
 {
-  size_global = points.size;
+  size_global = points->size;
   dim_kernel = 1;
   int i;
   unsigned int j;
@@ -44,49 +44,50 @@ void setup()
 
     for (i = 0; i < SIZE_X; i++)
     {
-      points.x[i + SIZE_X*j]= x;                                                // Setting "x" initial positions...
-      points.y[i + SIZE_X*j]= y;                                                // Setting "y" initial positions...
-      points.z[i + SIZE_X*j] = 0.0f;                                            // Setting "z" initial positions...
-      points.w[i + SIZE_X*j] = 1.0f;                                            // Setting "w" initial positions...
+      points->x[i + SIZE_X*j]= x;                                                // Setting "x" initial positions...
+      points->y[i + SIZE_X*j]= y;                                                // Setting "y" initial positions...
+      points->z[i + SIZE_X*j] = 0.0f;                                            // Setting "z" initial positions...
+      points->w[i + SIZE_X*j] = 1.0f;                                            // Setting "w" initial positions...
       x += DX;
     }
     y += DY;
   }
 
-  for (i = 0; i < colors.size; i++)
+  for (i = 0; i < colors->size; i++)
   {
-    colors.x[i] = glm::linearRand(0.0f, 1.0f);                                  // Setting "x" initial colors...
-    colors.y[i] = glm::linearRand(0.0f, 1.0f);                                  // Setting "y" initial colors...
-    colors.z[i] = glm::linearRand(0.0f, 1.0f);                                  // Setting "z" initial colors...
-    colors.w[i] = 1.0f;                                                         // Setting "w" initial colors...
+    colors->x[i] = glm::linearRand(0.0f, 1.0f);                                  // Setting "x" initial colors...
+    colors->y[i] = glm::linearRand(0.0f, 1.0f);                                  // Setting "y" initial colors...
+    colors->z[i] = glm::linearRand(0.0f, 1.0f);                                  // Setting "z" initial colors...
+    colors->w[i] = 1.0f;                                                         // Setting "w" initial colors...
   }
 
   tick = 0.0f;                                                                  // Setting initial time tick...
 
-  set_points(&points, 0);                                                       // Setting kernel argument #0...
-  set_colors(&colors, 1);                                                       // Setting kernel argument #1...
-  set_int(&points.size, 2);                                                     // Setting kernel argument #2...
+  set_points(points, 0);                                                       // Setting kernel argument #0...
+  set_colors(colors, 1);                                                       // Setting kernel argument #1...
+  set_int(&points->size, 2);                                                     // Setting kernel argument #2...
   set_float(&tick, 3);                                                          // Setting kernel argument #3...
 }
 
 void loop()
 {
-  push_points(&points.buffer);
-  push_colors(&colors.buffer);
-  push_int(&points.size, 2);
+  push_points(&points->buffer);
+  push_colors(&colors->buffer);
+  push_int(&points->size, 2);
   push_float(&tick, 3);
   push_kernel();
-  pop_points(&points.buffer);
-  pop_colors(&colors.buffer);
+  pop_points(&points->buffer);
+  pop_colors(&colors->buffer);
   pop_kernel();
-  //init_screen();
 
   tick += 0.1f;
 
-  plot_points(&points, &colors);
+  plot_points(points, colors);
 }
 
 void terminate()
 {
+  delete points;
+  delete colors;
   printf("All done!\n");
 }
