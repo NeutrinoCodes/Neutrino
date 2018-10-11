86342635d68d01381d4280221955391b66492ee0
Erik Zorzin <erik@zorzin.com>
2018-10-11 15:37:13 +0200

    Preparing for migration.

---
+++ Code/include/opengl.hpp
@@ -0,0 +1,132 @@
+/// @file
+
+#ifndef opengl_hpp
+#define opengl_hpp
+
+  #define SIZE_WINDOW_X 800                                                     // Window x-size [px].
+  #define SIZE_WINDOW_Y 600                                                     // Window y-size [px].
+  #define ZOOM_FACTOR 1.05f                                                     // Zoom factor [> 1.0].
+  #define ROTATION_FACTOR 2.0f                                                  // Rotation factor [].
+  #define NEAR_Z_CLIP 0.1f                                                      // Near z-clipping distance [> 0.0].
+  #define FAR_Z_CLIP 100.0f                                                     // Far z-clipping distance [< +inf].
+  #define FOV 60.0f                                                             // Field of view [deg].
+  #define LINE_WIDTH 3                                                          // Line width [px].
+
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <math.h>
+  #include <list.h>
+  #include <GL/glew.h>
+
+  #ifdef __WINDOWS__
+    #define GLFW_EXPOSE_NATIVE_WIN32
+    #define GLFW_EXPOSE_NATIVE_WGL
+  #endif
+
+  #ifdef __linux__
+    #define GLFW_EXPOSE_NATIVE_X11
+    #define GLFW_EXPOSE_NATIVE_GLX
+  #endif
+
+  #include <GLFW/glfw3.h>
+  #include <GLFW/glfw3native.h>
+
+  #ifdef __APPLE__
+    #include <OpenCL/opencl.h>
+    #include <OpenGL/OpenGL.h>
+  #else
+    #include <CL/cl.h>
+    #include <CL/cl_gl.h>
+    #include <GL/gl.h>
+  #endif
+
+  #include "linear_algebra.hpp"
+  #include "projective_geometry.hpp"
+  #include "utilities.hpp"
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////// "OPENGL" CLASS ///////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  class opengl
+  {
+    private:
+
+    public:
+            opengl();
+      void  init();                                                             // OpenGL initialization.
+            ~opengl();
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////// "WINDOW" CLASS ///////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  class window
+  {
+    private:
+      GLFWwindow*			    glfw_window;                                          // Window handle.
+      double              mouse_x_old;                                          // Mouse x-coordinate backup [px].
+      double              mouse_y_old;                                          // Mouse y-coordinate backup [px].
+      bool 			          arcball_on;                                           // Arcball activation flag.
+      float               R_old[16];                                            // Rotation matrix backup.
+      float               initial_translation[3];                               // Initial translation vector.
+
+      void grasp(float* p, int x, int y);                                       // Grasp arcball action.
+      void arcball();                                                           // Arcball computation.
+
+      // Key-pressed callback:
+      inline static auto key_pressed_callback(GLFWwindow* win, int key, int scancode, int action, int mods)->void
+      {
+        window* thewindow = static_cast<window*>(glfwGetUserPointer(win));
+        window->key_pressed(key, scancode, action, mods);
+      }
+
+      // Mouse-pressed callback:
+      inline static auto mouse_pressed_callback(GLFWwindow* win, int button, int action, int mods)->void
+      {
+        window* thewindow = static_cast<window*>(glfwGetUserPointer(win));
+        window->mouse_pressed(button, action, mods);
+      }
+
+      // Mouse-moved callback:
+      inline static auto mouse_moved_callback(GLFWwindow* win, double xpos, double ypos)->void
+      {
+        window* thewindow = static_cast<window*>(glfwGetUserPointer(win));
+        window->mouse_moved(xpos, ypos);
+      }
+
+      // Mouse-scrolled callback:
+      inline static auto mouse_scrolled_callback(GLFWwindow* win, double xoffset, double yoffset)->void
+      {
+        window* thewindow = static_cast<window*>(glfwGetUserPointer(win));
+        window->mouse_scrolled(xoffset, yoffset);
+      }
+
+    public:
+      int       window_sx;                                                      // Window x-size [px].
+      int       window_sy;                                                      // Window y-size [px].
+      double    aspect_ratio;                                                   // Window aspect ratio [].
+      double    mouse_x;                                                        // Mouse x-coordinate [px].
+      double    mouse_y;                                                        // Mouse y-coordinate [px].
+      double		scroll_x;                                                       // Scroll x-coordinate [px].
+      double		scroll_y;                                                       // Scroll y-coordinate [px].
+      double		zoom;                                                           // Zoom coefficient.
+
+      float     q[4];                                                           // Arcball quaternion.
+      float     R[16];                                                          // Rotation matrix.
+      float     T[16];                                                          // Translation matrix.
+      float     V[16];                                                          // View matrix.
+      float     P[16];                                                          // Projection matrix.
+
+            window(int window_size_x, int window_size_y, const char* title);
+      void  init();                                                             // Window initialization.
+      void  clear();                                                            // Window clearance.
+      void  refresh();                                                          // Window refresh.
+      auto  key_pressed(int key, int scancode, int action, int mods)->void;     // Key-pressed trampoline.
+      auto  mouse_pressed(int button, int action, int mods)->void;              // Mouse-pressed retpoline
+      auto  mouse_moved(double xpos, double ypos)->void;                        // Mouse-moved retpoline.
+      auto  mouse_scrolled(double xoffset, double yoffset)->void;               // Mouse-scrolled retpoline.
+            ~window();
+  };
+
+#endif
