42f2c8668f65f123fa9adde4b6f4f96e6823a484
ezor <erik@zorzin.com>
2018-10-30 17:29:17 +0100

    Restyling ex-data types.

--- Code/include/color4.hpp
+++ Code/include/color4.hpp
@@ -3,35 +3,70 @@
 #ifndef color4_hpp
 #define color4_hpp
 
-  #include <stdio.h>
+  #define LAYOUT_0 0
+  #define LAYOUT_1 1
 
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <math.h>
+  #include <GL/glew.h>
+
+  #ifdef __WINDOWS__
+    #define GLFW_EXPOSE_NATIVE_WIN32
+    #define GLFW_EXPOSE_NATIVE_WGL
+  #endif
+
+  #ifdef __linux__
+    #define GLFW_EXPOSE_NATIVE_X11
+    #define GLFW_EXPOSE_NATIVE_GLX
+  #endif
+
+  #include <GLFW/glfw3.h>
+  #include <GLFW/glfw3native.h>
+
+  #ifdef __APPLE__
+    #include <OpenGL/OpenGL.h>
+  #else
+    #include <GL/gl.h>
+  #endif
+
+  #ifdef __APPLE__
+    #include <OpenCL/opencl.h>
+  #else
+    #include <CL/cl.h>
+    #include <CL/cl_gl.h>
+  #endif
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////// "COLOR4" CLASS ///////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
   class color4
   {
     private:
-      int               err;
-      unsigned int      i;
-      GLfloat*          data;
-      GLuint            LAYOUT_1;                                               // "layout = 1" attribute in vertex shader.
-      cl_context        context;                                                // OpenCL context.
+      const char*     get_error(cl_int loc_error);
+      int             err;
+      unsigned int    i;
+      GLfloat*        data;
+      cl_context      context;                                                // OpenCL context.
 
     public:
-      color4(cl_context thecontext, int num_data);
-      ~color4();
+      GLfloat*        r;
+      GLfloat*        g;
+      GLfloat*        b;
+      GLfloat*        a;
 
-      GLfloat*  r;
-      GLfloat*  g;
-      GLfloat*  b;
-      GLfloat*  a;
+      int             size;
+      GLuint          vao;
+      GLuint          vbo;
+      cl_mem          buffer;
 
-      int       size;
-      GLuint    vao;
-      GLuint    vbo;
-      cl_mem    buffer;
-
-      void      init();
-      void      set(kernel* k, int kernel_arg);
-      void      push(queue* q, kernel* k, int kernel_arg);
-      void      pop(queue* q, kernel* k, int kernel_arg);
+                      color4();
+      void            init(int num_data);
+      void            set(kernel* k, int kernel_arg);
+      void            push(queue* q, kernel* k, int kernel_arg);
+      void            pop(queue* q, kernel* k, int kernel_arg);
+                      ~color4();
   };
 
 #endif
--- Code/include/int1.hpp
+++ Code/include/int1.hpp
@@ -3,15 +3,50 @@
 #ifndef int1_hpp
 #define int1_hpp
 
-  #include <stdio.h>
+  #define LAYOUT_0 0
+  #define LAYOUT_1 1
 
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <math.h>
+  #include <GL/glew.h>
+
+  #ifdef __WINDOWS__
+    #define GLFW_EXPOSE_NATIVE_WIN32
+    #define GLFW_EXPOSE_NATIVE_WGL
+  #endif
+
+  #ifdef __linux__
+    #define GLFW_EXPOSE_NATIVE_X11
+    #define GLFW_EXPOSE_NATIVE_GLX
+  #endif
+
+  #include <GLFW/glfw3.h>
+  #include <GLFW/glfw3native.h>
+
+  #ifdef __APPLE__
+    #include <OpenGL/OpenGL.h>
+  #else
+    #include <GL/gl.h>
+  #endif
+
+  #ifdef __APPLE__
+    #include <OpenCL/opencl.h>
+  #else
+    #include <CL/cl.h>
+    #include <CL/cl_gl.h>
+  #endif
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////// "INT1" CLASS ////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
   class int1
   {
     private:
+      const char*     get_error(cl_int loc_error);
       int               err;
       unsigned int      i;
-      // NOTE: There is no need for "unfolded-data" because these data are 1xN.
-      GLuint            LAYOUT_0;                                               // "layout = 0" attribute in vertex shader.
       cl_context        context;                                                // OpenCL context.
 
     public:
