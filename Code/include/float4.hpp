43af35b53dcebd7a5a82a77268cc09b65ef35b29
ezor <erik@zorzin.com>
2018-10-30 16:10:01 +0100

    Transferred everything was in data_types in corresponding new files.

--- Code/include/float4.hpp
+++ Code/include/float4.hpp
@@ -5,4 +5,33 @@
 
   #include <stdio.h>
 
+  class float4
+  {
+    private:
+      int               err;                                                    // Declaring "error" code...
+      unsigned int      i;                                                      // Declaring "i" index...
+      GLfloat*          data;                                                   // Declaring unfolded data array...
+      GLuint            LAYOUT_0;                                               // "layout = 0" attribute in vertex shader.
+      cl_context        context;                                                // OpenCL context.
+
+    public:
+      float4(cl_context thecontext, int num_data);
+      ~float4();
+
+      cl_float*    x;                                                           // Declaring "x" data...
+      cl_float*    y;                                                           // Declaring "y" data...
+      cl_float*    z;                                                           // Declaring "z" data...
+      cl_float*    w;                                                           // Declaring "w" data...
+
+      int       size;                                                           // Declaring "size" [#]...
+      GLuint    vao;
+      GLuint    vbo;
+      cl_mem    buffer;                                                         // Declaring OpenCL memory buffer...
+
+      void      init();
+      void      set(kernel* k, int kernel_arg);
+      void      push(queue* q, kernel* k, int kernel_arg);
+      void      pop(queue* q, kernel* k, int kernel_arg);
+  };
+
 #endif
