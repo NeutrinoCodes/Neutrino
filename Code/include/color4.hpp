d4b22520da7e13fa58e2782f3dc2cde58b7e8ac7
Erik Zorzin <erik@zorzin.com>
2018-11-03 11:56:38 +0100

    Beatifying code.

--- Code/include/color4.hpp
+++ Code/include/color4.hpp
@@ -13,51 +13,56 @@
   class color4
   {
     private:
-      // OpenCL error function:
+      // OpenCL error get function:
       const char*         get_error     (
-                                          cl_int loc_error                      // Local error code.
+                                          cl_int      loc_error                 // Local error code.
                                         );
 
+      // OpenCL error check function:
       void                check_error   (
-                                          cl_int loc_error                      // Local error code.
+                                          cl_int      loc_error                 // Local error code.
                                         );
 
-      GLfloat*            data;
+      GLfloat*            data;                                                 // Wrapped data.
       cl_context          opencl_context;                                       // OpenCL context.
 
     public:
-      GLfloat*            r;
-      GLfloat*            g;
-      GLfloat*            b;
-      GLfloat*            a;
+      GLfloat*            r;                                                    // Red color channel.
+      GLfloat*            g;                                                    // Green color channel.
+      GLfloat*            b;                                                    // Blue color channel.
+      GLfloat*            a;                                                    // Alpha transparency channel.
 
-      cl_ulong            size;
-      GLuint              vao;
-      GLuint              vbo;
-      cl_mem              buffer;
+      cl_ulong            size;                                                 // Data size.
+      GLuint              vao;                                                  // OpenGL data VAO.
+      GLuint              vbo;                                                  // OpenGL data VBO.
+      cl_mem              buffer;                                               // OpenGL data memory buffer.
 
                           color4();
 
+      // Initialization:
       void                init          (
-                                          neutrino* loc_neutrino,
-                                          cl_ulong loc_data_number
+                                          neutrino*   loc_neutrino,             // Neutrino baseline.
+                                          cl_ulong    loc_data_number           // Data size.
                                         );
 
+      // Set kernel argument:
       void                set           (
-                                          kernel* loc_kernel,
-                                          cl_ulong loc_kernel_arg
+                                          kernel*     loc_kernel,               // OpenCL kernel.
+                                          cl_ulong    loc_kernel_arg            // OpenCL kernel argument index.
                                         );
 
+      // Push kernel argument:
       void                push          (
-                                          queue* loc_queue,
-                                          kernel* loc_kernel,
-                                          cl_ulong loc_kernel_arg
+                                          queue*      loc_queue,                // OpenCL queue.
+                                          kernel*     loc_kernel,               // OpenCL kernel.
+                                          cl_ulong    loc_kernel_arg            // OpenCL kernel argument index.
                                         );
 
+      // Pop kernel argument:
       void                pop           (
-                                          queue* loc_queue,
-                                          kernel* loc_kernel,
-                                          cl_ulong loc_kernel_arg
+                                          queue*      loc_queue,                // OpenCL queue.
+                                          kernel*     loc_kernel,               // OpenCL kernel.
+                                          cl_ulong    loc_kernel_arg            // OpenCL kernel argument index.
                                         );
 
                           ~color4();
