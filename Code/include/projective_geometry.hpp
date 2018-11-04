258aacd36f586a90bff241e15f02f7a11d213481
Erik Zorzin <erik@zorzin.com>
2018-11-04 13:05:44 +0100

    debugging includes.

--- Code/include/projective_geometry.hpp
+++ Code/include/projective_geometry.hpp
@@ -3,6 +3,15 @@
 #ifndef projective_geometry_hpp
 #define projective_geometry_hpp
 
+  //////////////////////////////////////////////////////////////////////////
+  //////////////////////// Standard C header files /////////////////////////
+  //////////////////////////////////////////////////////////////////////////
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <math.h>
+  #include <errno.h>
+
   void frustum(float F[16], float left, float right, float bottom,
                   float top, float z_near, float z_far);
   void perspective(float P[16], float fov, float aspect_ratio,
