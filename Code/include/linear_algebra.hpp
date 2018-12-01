ec5b42fd8b69f9a2eae15dea5264dabfe4f1e49d
Erik Zorzin <erik@zorzin.com>
2018-12-01 21:57:36 +0100

    Beautifying and commenting code.

--- Code/include/linear_algebra.hpp
+++ Code/include/linear_algebra.hpp
@@ -5,22 +5,39 @@
 
 #include "neutrino.hpp"
 
-  /// **Dot product of two 3D vectors:**
-  float   dot(float a[3], float b[3]);
+  // Dot product of two 3D vectors:
+  float   dot             (
+                            float a[3],                                         // 3D vector.
+                            float b[3]                                          // 3D vector.
+                          );
 
-  /// **Magnitude of a 3D vector:**
-  float   magnitude(float a[3]);
+  // Magnitude of a 3D vector:
+  float   magnitude       (
+                            float a[3]                                          // 3D vector.
+                          );
 
-  /// **Normalization of a 3D vector:**
-  void    normalize(float a[3]);
+  // Normalization of a 3D vector:
+  void    normalize       (
+                            float a[3]                                          // 3D vector.
+                          );
 
-  /// **Angle between two 3D vectors:**
-  float   angle(float a[3], float b[3]);
+  // Angle between two 3D vectors:
+  float   angle           (
+                            float a[3],                                         // 3D vector.
+                            float b[3]                                          // 3D vector.
+                          );
 
-  /// **Cross product between two 3D vectors:***
-  void    cross(float v[3], float a[3], float b[3]);
+  // Cross product between two 3D vectors:
+  void    cross           (
+                            float v[3],                                         // 3D cross product vector.
+                            float a[3],                                         // 3D vector.
+                            float b[3]                                          // 3D vector.
+                          );
 
-  /// **Associated 3x3 matrix representing the quaternion rotation:**
-  void    rotation_matrix(float M[9], float q[4]);
+  // Associated 3x3 matrix representing the quaternion rotation:
+  void    rotation_matrix (
+                            float M[9],                                         // 3x3 rotation matrix.
+                            float q[4]                                          // Rotation quaternion.
+                          );
 
 #endif
