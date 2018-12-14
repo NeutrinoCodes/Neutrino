8ea4d18be5d5b609532d2f1115204bb569283fc5
ezor <erik@zorzin.com>
2018-12-14 12:11:15 +0100

    int4: done!

--- Code/include/int4.hpp
+++ Code/include/int4.hpp
@@ -14,6 +14,7 @@
   {
     private:
       neutrino*           baseline;                                             // Neutrino baseline.
+      size_t*             position;                                             // Position of kernel argument in each kernel.
 
       // OpenCL error get function:
       const char*         get_error     (
@@ -38,68 +39,84 @@
 
       // Initialization:
       void                init          (
-                                          neutrino* loc_baseline,               // Neutrino baseline.
-                                          kernel*   loc_kernel,                 // OpenCL kernel.
-                                          cl_uint   loc_kernel_arg,             // OpenCL kernel argument #.
-                                          size_t    loc_data_size               // Data number.
+                                          neutrino*   loc_baseline,             // Neutrino baseline.
+                                          GLsizeiptr  loc_data_size             // Data number.
+                                        );
+
+      ////////////////////////////////////////////////////////////////////////////
+      /////////////////////////// HOST "SET" FUNCTIONS:  /////////////////////////
+      ////////////////////////////////////////////////////////////////////////////
+      // Kernel set function:
+      void                set_arg       (
+                                          kernel*     loc_kernel,               // OpenCL kernel.
+                                          cl_uint     loc_kernel_arg            // OpenCL kernel argument #.
                                         );
 
       // "x" set function:
       void                set_x         (
-                                          size_t    loc_index,                  // Data index.
-                                          cl_long   loc_value                   // Data value.
+                                          size_t      loc_index,                // Data index.
+                                          cl_long     loc_value                 // Data value.
                                         );
 
       // "y" set function:
       void                set_y         (
-                                          size_t    loc_index,                  // Data index.
-                                          cl_long   loc_value                   // Data value.
+                                          size_t      loc_index,                // Data index.
+                                          cl_long     loc_value                 // Data value.
                                         );
 
       // "z" set function:
       void                set_z         (
-                                          size_t    loc_index,                  // Data index.
-                                          cl_long   loc_value                   // Data value.
+                                          size_t      loc_index,                // Data index.
+                                          cl_long     loc_value                 // Data value.
                                         );
 
       // "w" set function:
       void                set_w         (
-                                          size_t    loc_index,                  // Data index.
-                                          cl_long   loc_value                   // Data value.
+                                          size_t      loc_index,                // Data index.
+                                          cl_long     loc_value                 // Data value.
+                                        );
+
+      ////////////////////////////////////////////////////////////////////////////
+      /////////////////////////// HOST "GET" FUNCTIONS:  /////////////////////////
+      ////////////////////////////////////////////////////////////////////////////
+      // Kernel get function:
+      size_t              get_arg       (
+                                          kernel*     loc_kernel                // OpenCL kernel.
                                         );
 
       // "x" get function:
       cl_long             get_x         (
-                                          size_t    loc_index                   // Data index.
+                                          size_t      loc_index                 // Data index.
                                         );
 
       // "y" get function:
       cl_long             get_y         (
-                                          size_t    loc_index                   // Data index.
+                                          size_t      loc_index                 // Data index.
                                         );
 
       // "z" get function:
       cl_long             get_z         (
-                                          size_t    loc_index                   // Data index.
+                                          size_t      loc_index                 // Data index.
                                         );
 
       // "w" get function:
       cl_long             get_w         (
-                                          size_t    loc_index                   // Data index.
+                                          size_t      loc_index                 // Data index.
                                         );
 
+      ////////////////////////////////////////////////////////////////////////////
+      ///////////////////////////// CLIENT FUNCTIONS:  ///////////////////////////
+      ////////////////////////////////////////////////////////////////////////////
       // OpenCL write buffer function:
-      void                write         (
-                                          queue*    loc_queue,                  // OpenCL queue.
-                                          kernel*   loc_kernel,                 // OpenCL kernel.
-                                          cl_uint   loc_kernel_arg              // OpenCL kernel argument index.
+      void                push          (
+                                          queue*      loc_queue,                // OpenCL queue.
+                                          cl_uint     loc_kernel_arg            // OpenCL kernel argument index.
                                         );
 
       // OpenCL read buffer function:
-      void                read          (
-                                          queue*    loc_queue,                  // OpenCL queue.
-                                          kernel*   loc_kernel,                 // OpenCL kernel.
-                                          cl_uint   loc_kernel_arg              // OpenCL kernel argument index.
+      void                pull          (
+                                          queue*      loc_queue,                // OpenCL queue.
+                                          cl_uint     loc_kernel_arg            // OpenCL kernel argument index.
                                         );
 
                           ~int4();
