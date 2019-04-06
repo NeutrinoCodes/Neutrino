9069756d68710f0cb7c9a00bb929153ba131aa07
Erik Zorzin <erik@zorzin.com>
2019-04-06 23:36:13 +0200

    Making point arrays. Kind of working but there is a bug. Debugging.

--- Code/include/projective_geometry.hpp
+++ Code/include/projective_geometry.hpp
@@ -7,30 +7,30 @@
   #include "linear_algebra.hpp"
 // Copies "old" matrix into "new" matrix.
 void backup (
-             float M[16],                                                       // New matrix: takes values from old matrix.
-             float M_old[16]                                                    // Old matrix.
+             float M_mat[16],                                                   // New matrix: takes values from old matrix.
+             float M_mat_old[16]                                                // Old matrix.
             );
 // Multiplication matrix computation:
 void multiplicate (
-                   float C[16],                                                 // 4x4 matrix result.
-                   float A[16],                                                 // 4x4 matrix.
-                   float B[16]                                                  // 4x4 matrix.
+                   float C_mat[16],                                             // 4x4 matrix result.
+                   float A_mat[16],                                             // 4x4 matrix.
+                   float B_mat[16]                                              // 4x4 matrix.
                   );
 // Translation matrix computation:
 void translate (
-                float T[16],                                                    // 4x4 translation matrix.
-                float T_old[16],                                                // 4x4 translation matrix backup.
+                float T_mat[16],                                                // 4x4 translation matrix.
+                float T_mat_old[16],                                            // 4x4 translation matrix backup.
                 float t[3]                                                      // 3D translation vector.
                );
 // Rotation matrix computation:
 void rotate (
-             float R[16],                                                       // 4x4 rotation matrix.
-             float R_old[16],                                                   // 4x4 rotation matrix backup.
+             float R_mat[16],                                                   // 4x4 rotation matrix.
+             float R_mat_old[16],                                               // 4x4 rotation matrix backup.
              float q[4]                                                         // Rotation quaternion.
             );
 // Perspective frustum computation:
 void frustum (
-              float F[16],                                                      // 4x4 frustum matrix.
+              float F_mat[16],                                                  // 4x4 frustum matrix.
               float left,                                                       // Projective screen left side...
               float right,                                                      // Projective screen right side...
               float bottom,                                                     // Projective screen bottom side...
@@ -40,7 +40,7 @@ void frustum (
              );
 // Perspective matrix computation:
 void perspective_mono (
-                       float P[16],                                             // 4x4 perspective matrix.
+                       float P_mat[16],                                         // 4x4 perspective matrix.
                        float fov,                                               // Field of view [rad].
                        float aspect_ratio,                                      // Projective screen aspect ratio.
                        float z_near,                                            // Projective screen near depth...
@@ -50,10 +50,10 @@ void perspective_mono (
 /// ### Description:
 /// Computes the perspective matrix given the projective screen boundaries.
 void perspective_stereo (
-                         float PL[16],                                          // 4x4 right eye perspective matrix.
-                         float PR[16],                                          // 4x4 left eye perspective matrix.
-                         float TL[16],                                          // 4x4 right eye translation matrix.
-                         float TR[16],                                          // 4x4 left eye translation matrix.
+                         float PL_mat[16],                                      // 4x4 right eye perspective matrix.
+                         float PR_mat[16],                                      // 4x4 left eye perspective matrix.
+                         float TL_mat[16],                                      // 4x4 right eye translation matrix.
+                         float TR_mat[16],                                      // 4x4 left eye translation matrix.
                          float iod,                                             // Intraocular distance.
                          float fov,                                             // Field of view [rad].
                          float aspect_ratio,                                    // Projective screen aspect ratio.
