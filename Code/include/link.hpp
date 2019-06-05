2cd111d1832afcf8d565275b564dc372f4f9b031
ezor <erik@zorzin.com>
2019-06-05 11:42:21 +0200

    Separating nodes from links.

---
+++ Code/include/link.hpp
@@ -0,0 +1,12 @@
+#ifndef link_hpp
+#define link_hpp
+
+#include "neutrino.hpp"
+#include "opengl.hpp"
+#include "opencl.hpp"
+#include "kernel.hpp"
+#include "queue.hpp"
+#include "data.hpp"
+
+
+#endif
---
+++ Code/include/node.hpp
@@ -0,0 +1,13 @@
+#ifndef node_hpp
+#define node_hpp
+
+#include "neutrino.hpp"
+#include "opengl.hpp"
+#include "opencl.hpp"
+#include "kernel.hpp"
+#include "queue.hpp"
+#include "data.hpp"
+
+
+
+#endif
