41f5eb092373b0c28de1062f2a0714adfdf9cb15
Erik Zorzin <erik@zorzin.com>
2019-06-23 00:02:05 +0200

    Introducing variadic function template "init" in shader.hpp.

--- Code/include/shader.hpp
+++ Code/include/shader.hpp
@@ -10,7 +10,6 @@ class shader
 {
 private:
   neutrino* baseline;                                                           // Neutrino baseline.
-
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////////// PRIVATE METHODS ///////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -36,9 +35,59 @@ public:
   ////////////////////////////////////////////////////////////////////////////////
   shader ();
 
+  ////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////// INIT /////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  template <typename T, typename ... Args>
+  GLuint init (
+               const char* loc_vertex_filename,                                 // Vertex shader file name.
+               const char* loc_geometry_filename,                               // Geometry shader file name.
+               const char* loc_fragment_filename,                               // Fragment shader file name.
+               const char* loc_var_name,                                        // Vertex shader 1st input variable name.
+               Args...     args                                                 // Vertex shader extra input variable names.
+              )
+  {
+    GLuint vertex;                                                              // Vertex shader.
+    GLuint geometry;                                                            // Geometry shader.
+    GLuint fragment;                                                            // Fragment shader.
+    GLuint program;                                                             // Shader program.
+    size_t i = 0;                                                               // Argument counter;
 
-  ////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////// DESTRUCTOR ////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+    if(i == 0)
+    {
+      vertex   = compile_shader (loc_vertex_filename, VERTEX);                  // Compiling vertex shader...
+      geometry = compile_shader (loc_geometry_filename, GEOMETRY);              // Compiling geometry shader...
+      fragment = compile_shader (loc_fragment_filename, FRAGMENT);              // Compiling fragment shader...
+
+      program  = glCreateProgram ();                                            // Creating program...
+
+      glAttachShader (program, vertex);                                         // Attaching vertex shader to program...
+      glAttachShader (program, geometry);                                       // Attaching geometry shader to program...
+      glAttachShader (program, fragment);                                       // Attaching fragment shader to program...
+
+      glLinkProgram (program);                                                  // Linking program...
+
+      return (program);                                                         // Returning shader program...
+    }
+
+    i++;
+
+    glBindAttribLocation (program, 0, loc_var_name);                            // Binding point...
+    init (args ...);
+
+    if( i == sizeof...(args))
+    {
+      glAttachShader (program, vertex);                                         // Attaching vertex shader to program...
+      glAttachShader (program, geometry);                                       // Attaching geometry shader to program...
+      glAttachShader (program, fragment);                                       // Attaching fragment shader to program...
+      glLinkProgram (program);                                                  // Linking program...
+
+      return (program);                                                         // Returning shader program...
+    }
+  };
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// DESTRUCTOR ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
   ~shader ();
 #endif
