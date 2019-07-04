da14744fb36d408bdaf0c3d481b8d096cd941f4b
ezor <erik@zorzin.com>
2019-07-04 10:55:26 +0200

    Debugging main: problems with the structs.

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -5,9 +5,9 @@
  #define XMAX        +1.0                                                       // XMAX spatial boundary [m].
  #define YMIN        -1.0                                                       // YMIN spatial boundary [m].
  #define YMAX        +1.0                                                       // YMAX spatial boundary [m].
- #define NODES_X     100                                                        // Number of nodes in "X" direction [#].
- #define NODES_Y     100                                                        // Number of nodes in "Y" direction [#].
- #define NODES       10000                                                      // To be fixed: (NODES_X)*(NODES_Y)                           // Total number of nodes [#].
+ #define NODES_X     10                                                        // Number of nodes in "X" direction [#].
+ #define NODES_Y     10                                                        // Number of nodes in "Y" direction [#].
+ #define NODES       100                                                      // To be fixed: (NODES_X)*(NODES_Y)                           // Total number of nodes [#].
  #define DX          (float)((XMAX - XMIN)/(NODES_X - 1))                       // DX mesh spatial size [m].
  #define DY          (float)((YMAX - YMIN)/(NODES_Y - 1))                       // DY mesh spatial size [m].
 
@@ -16,10 +16,10 @@
  #define GUI_SIZE_X  800                                                        // Window x-size [px].
  #define GUI_SIZE_Y  600                                                        // Window y-size [px].
  #define GUI_NAME    "neutrino 3.0"                                             // Window name.
-//#define SHADER_HOME \
-//  "/run/media/ezor/LINUX/BookhouseBoys/ezor/Neutrino/Code/shader"
- #define SHADER_HOME \
-  "/Users/Erik/Documents/PROJECTS/BookhouseBoys/ezor/Neutrino/Code/shader"
+#define SHADER_HOME \
+  "/run/media/ezor/LINUX/BookhouseBoys/ezor/Neutrino/Code/shader"
+ //#define SHADER_HOME \
+//  "/Users/Erik/Documents/PROJECTS/BookhouseBoys/ezor/Neutrino/Code/shader"
  #define SHADER_VERT "voxel_vertex.vert"                                        // OpenGL vertex shader.
  #define SHADER_GEOM "voxel_geometry.geom"                                      // OpenGL geometry shader.
  #define SHADER_FRAG "voxel_fragment.frag"                                      // OpenGL fragment shader.
@@ -30,10 +30,10 @@
  #define KERNEL_SX   NODES                                                      // Dimension of OpenCL kernel (i-index).
  #define KERNEL_SY   0                                                          // Dimension of OpenCL kernel (j-index).
  #define KERNEL_SZ   0                                                          // Dimension of OpenCL kernel (k-index).
-//#define KERNEL_HOME \
-//  "/run/media/ezor/LINUX/BookhouseBoys/ezor/Neutrino/Code/kernel"               // OpenCL kernel header files directory.
 #define KERNEL_HOME \
-  "/Users/Erik/Documents/PROJECTS/BookhouseBoys/ezor/Neutrino/Code/kernel"
+  "/run/media/ezor/LINUX/BookhouseBoys/ezor/Neutrino/Code/kernel"               // OpenCL kernel header files directory.
+//#define KERNEL_HOME \
+//  "/Users/Erik/Documents/PROJECTS/BookhouseBoys/ezor/Neutrino/Code/kernel"
  #define KERNEL_FILE "sine_kernel.cl"                                           // OpenCL kernel.
 
 // INCLUDES:
@@ -71,32 +71,35 @@ int main ()
   P->name = "voxel_center";
   C->name = "voxel_color";
 
+  P->data[1].x = 1.0f;
+
   for(j = 0; j < NODES_Y; j++)
   {
     for(i = 0; i < NODES_X; i++)
     {
+      printf("%d\n", j*NODES_X + i);
       // Setting point coordinates:
-      P->data[j*NODES_X + i].x = i*DX + XMIN;
-      P->data[j*NODES_X + i].y = j*DY + YMIN;
-      P->data[j*NODES_X + i].z = 0.0;
-      P->data[j*NODES_X + i].w = 1.0;
+      //P->data[j*NODES_X + i].x = i*DX + XMIN;
+      //P->data[j*NODES_X + i].y = j*DY + YMIN;
+      //P->data[j*NODES_X + i].z = 0.0;
+      //P->data[j*NODES_X + i].w = 1.0;
 
       // Setting point colors:
-      C->data[j*NODES_X + i].r = 0.01*(rand () % 100);
-      C->data[j*NODES_X + i].g = 0.01*(rand () % 100);
-      C->data[j*NODES_X + i].b = 0.01*(rand () % 100);
-      C->data[j*NODES_X + i].a = 1.0;
+      //C->data[j*NODES_X + i].r = 0.01*(rand () % 100);
+      //C->data[j*NODES_X + i].g = 0.01*(rand () % 100);
+      //C->data[j*NODES_X + i].b = 0.01*(rand () % 100);
+      //C->data[j*NODES_X + i].a = 1.0;
     }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////// SETTING OPENCL KERNEL ARGUMENTS ///////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-  K->setarg (P, 0);                                                             // Setting kernel argument...
-  Q->write (P, 0);                                                              // Uploading data on kernel...
+  //K->setarg (P, 0);                                                             // Setting kernel argument...
+  //Q->write (P, 0);                                                              // Uploading data on kernel...
 
-  K->setarg (C, 1);                                                             // Setting kernel argument...
-  Q->write (C, 1);                                                              // Uploading data on kernel...
+  //K->setarg (C, 1);                                                             // Setting kernel argument...
+  //Q->write (C, 1);                                                              // Uploading data on kernel...
 
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////// SETTING OPENGL SHADER ARGUMENTS ///////////////////////
@@ -114,11 +117,11 @@ int main ()
     gui->clear ();                                                              // Clearing gui...
     gui->poll_events ();                                                        // Polling gui events...
 
-    Q->acquire (P, 0);                                                          // Acquiring OpenGL/CL shared argument...
-    Q->acquire (C, 1);                                                          // Acquiring OpenGL/CL shared argument...
-    ctx->execute (K, Q, WAIT);                                                  // Executing OpenCL kenrnel...
-    Q->release (P, 0);                                                          // Releasing OpenGL/CL shared argument...
-    Q->release (C, 1);                                                          // Releasing OpenGL/CL shared argument...
+    //Q->acquire (P, 0);                                                          // Acquiring OpenGL/CL shared argument...
+    //Q->acquire (C, 1);                                                          // Acquiring OpenGL/CL shared argument...
+    //ctx->execute (K, Q, WAIT);                                                  // Executing OpenCL kenrnel...
+    //Q->release (P, 0);                                                          // Releasing OpenGL/CL shared argument...
+    //Q->release (C, 1);                                                          // Releasing OpenGL/CL shared argument...
 
     gui->plot (S);                                                              // Plotting shared arguments...
     gui->refresh ();                                                            // Refreshing gui...
