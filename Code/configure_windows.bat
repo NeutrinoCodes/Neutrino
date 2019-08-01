ab2c281f8fe6d18dead1a8eda147c98d3c25485f
ezor <erik@zorzin.com>
2019-08-01 17:24:42 +0200

    It compiles and run on Windows but it stops because it cannot find the shader's files. maybe a problem of the path?

--- Code/configure_windows.bat
+++ Code/configure_windows.bat
@@ -1,1 +1,1 @@
-cmake -B"./cmake" -H"./" -DGLFW_PATH=C:/GLFW -DGLAD_PATH=C:/GLAD -DCL_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1/"
+cmake -B"./cmake" -H"./" -DGLFW_PATH="C:/GLFW" -DGLAD_PATH="C:/GLAD" -DCL_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1" -DCMAKE_GENERATOR_PLATFORM=x64
