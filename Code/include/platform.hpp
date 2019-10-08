9d5a7c55c05fb14fbabf92db8bea2f7c610a8337
Erik Zorzin <erik@zorzin.com>
2019-10-08 19:16:34 +0200

    "name" was missing in platform.hpp. I don't know how this is possible. Fixed.

--- Code/include/platform.hpp
+++ Code/include/platform.hpp
@@ -1,7 +1,9 @@
-/// @file   platform.hpp
-/// @author Erik ZORZIN
-/// @date   24OCT2019
-/// @brief  Declaration of an OpenCL "platform" class.
+/// @file     platform.hpp
+/// @author   Erik ZORZIN
+/// @date     24OCT2019
+/// @brief    Declaration of an OpenCL "platform" class.
+///
+/// @details  Declares methods in order to retrieve information about an OpenCL platform.
 
 #ifndef platform_hpp
 #define platform_hpp
@@ -9,39 +11,49 @@
   #include "neutrino.hpp"
   #include "data_classes.hpp"
 
-////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////// "PLATFORM" CLASS /////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////// "platform" class ////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 class platform
 {
 private:
-  neutrino*   baseline;                                                                             // Neutrino baseline.
-  std::string parameter;                                                                            // Parameter value.
+  neutrino*   baseline;                                                                             ///< @brief **Neutrino baseline.**
+  std::string parameter;                                                                            ///< @brief **Parameter value.**
+
   // Gets size information:
   size_t      get_info_size (
-                             cl_platform_id   loc_platform_id,                                      // Platform ID.
-                             cl_platform_info loc_parameter_name                                    // Paramenter name.
+                             cl_platform_id   loc_platform_id,                                      ///< Platform ID.
+                             cl_platform_info loc_parameter_name                                    ///< Paramenter name.
                             );
+
   // Gets size information:
   std::string get_info_value (
-                              cl_platform_id   loc_platform_id,                                     // Platform ID.
-                              cl_platform_info loc_parameter_name,                                  // Paramenter name.
-                              size_t           loc_parameter_size                                   // Parameter size.
+                              cl_platform_id   loc_platform_id,                                     ///< Platform ID.
+                              cl_platform_info loc_parameter_name,                                  ///< Paramenter name.
+                              size_t           loc_parameter_size                                   ///< Parameter size.
                              );
 
 public:
-  cl_platform_id id;                                                                                // OpenCL platform id.
-  std::string    profile;                                                                           // Platform parameter.
-  std::string    version;                                                                           // Platform parameter.
-  std::string    name;                                                                              // Platform parameter.
-  std::string    vendor;                                                                            // Platform parameter.
-  std::string    extensions;                                                                        // Platform parameter.
+  cl_platform_id id;                                                                                ///< @brief **OpenCL platform id.**
+  std::string    profile;                                                                           ///< @brief **Platform parameter.**
+  std::string    version;                                                                           ///< @brief **Platform parameter.**
+  std::string    name;                                                                              ///< @brief **Platform parameter.**
+  std::string    vendor;                                                                            ///< @brief **Platform parameter.**
+  std::string    extensions;                                                                        ///< @brief **Platform parameter.**
 
+  /// @brief **Class constructor.**
+  /// @details It does nothing.
   platform  ();
+
+  /// @brief **Class initializer.**
+  /// @details It gets all information about the selected OpenCL platform and store it in the
+  /// corresponding public variables.
   void init (
-             cl_platform_id loc_platform_id                                                         // Platform ID.
+             cl_platform_id loc_platform_id                                                         ///< Platform ID.
             );
 
+  /// @brief **Class destructor.**
+  /// @details It does nothing.
   ~platform ();
 };
 
