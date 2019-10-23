93c04c74174f96ca38880eac743b2b1360cfc904
ezorzin <erik@zorzin.com>
2019-10-23 10:13:57 +0200

    Added note in README.md in order to compile with MSBuild. However, Visual Studio still need to be installed. Tested under Windows 10.

--- Code/configure_windows.bat
+++ Code/configure_windows.bat
@@ -1,2 +1,1 @@
-cmake -B"./cmake" -H"./" -DGLFW_PATH="C:/GLFW" -DGLAD_PATH="C:/GLAD" -DCL_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1" -DCMAKE_C_COMPILER="msbuild" -DCMAKE_CXX_COMPILER="msbuild"
--G "Visual Studio 16 2019"
+cmake -B"./cmake" -H"./" -DGLFW_PATH="C:/GLFW" -DGLAD_PATH="C:/GLAD" -DCL_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1" -DCMAKE_GENERATOR_PLATFORM=x64
