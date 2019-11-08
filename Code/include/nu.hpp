4ad9244b98a655f136b588e4a961fafdabf9eeaf
Erik Zorzin <erik@zorzin.com>
2019-11-08 11:38:27 +0100

    Trying building Neutrino as a library.

---
+++ Code/include/nu.hpp
@@ -0,0 +1,14 @@
+/// @file     nu.hpp
+/// @author   Erik ZORZIN
+/// @date     08NOV2019
+/// @brief    Neutrino library header file.
+///
+/// @details  The Neutrino library header file.
+
+#ifndef nu_hpp
+#define nu_hpp
+
+#include "opengl.hpp"                                                                               // OpenGL.
+#include "opencl.hpp"                                                                               // OpenCL.
+
+#endif
