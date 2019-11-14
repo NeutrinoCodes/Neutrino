6221c27e019959d8fe824c720106789c812d15b2
ezor <erik@zorzin.com>
2019-11-14 12:35:29 +0100

    Added nu.hpp.

---
+++ Code/include/nu.hpp
@@ -0,0 +1,16 @@
+/// @file     nu.hpp
+/// @author   Erik ZORZIN
+/// @date     14NOV2019
+/// @brief    Header file for Neutrino library.
+///
+/// @details  This is the header file that must be included in all user's
+/// Neutrino based applications.
+
+#ifndef nu_hpp
+#define nu_hpp
+
+// INCLUDES:
+  #include "opengl.hpp"                                                                             // Neutrino's OpenGL context declarations.
+  #include "opencl.hpp"                                                                             // Neutrino's OpenCL context declarations.
+
+#endif
