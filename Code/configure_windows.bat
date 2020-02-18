89305d97728ef6964f81286c8a5a05b1ad69d8a7
Erik Zorzin <erik@zorzin.com>
2020-02-18 14:22:44 +0100

    Fixed README.md, removed unnecessary configure_winddows.bat.

--- Code/configure_windows.bat
+++
@@ -1,5 +0,0 @@
-cmake -B"./cmake" -H"./" -G"Unix Makefiles" ^
--DGLFW_PATH="F:/BookHouseBoys/ezor/NeutrinoCodes/glfw" ^
--DGLAD_PATH="F:/BookHouseBoys/ezor/NeutrinoCodes/glad" ^
--DCL_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1" ^
--DNEUTRINO_PATH="F:/BookHouseBoys/ezor/NeutrinoCodes/libnu"
