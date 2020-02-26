48c1c1ff3a06dc203cd47b326900d8ac251ff720
ezorzin <erik@zorzin.com>
2020-02-26 11:04:06 +0100

    Fixed mouse bug. Tested on Windows.

--- Code/src/device.cpp
+++ Code/src/device.cpp
@@ -493,15 +493,11 @@ void device::init
 {
   glFinish ();                                                                                      // Waiting for OpenGL to finish...
 
-  std::cout << "pippo" << std::endl;
-
   address_bits                  = get_cl_uint                                                       // Getting device information...
                                   (
                                    loc_device_id,                                                   // OpenCL device id [cl_device_id].
                                    CL_DEVICE_ADDRESS_BITS                                           // Parameter of interest [cl_device_info].
                                   );
-
-  std::cout << "puppo" << std::endl;
 
   available                     = get_cl_bool                                                       // Getting device information...
                                   (
@@ -762,8 +758,6 @@ void device::init
                                   );
 
   id                            = loc_device_id;                                                    // Initializing device_id...
-
-  std::cout << "peppo" << std::endl;
 }
 
 device::~device()
