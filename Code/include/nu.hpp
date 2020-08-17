89a80d9239acdc952c063e331facbb0ce162eef8
Erik Zorzin <erik@zorzin.com>
2020-08-17 12:55:58 +0200

    Debugging mesh with more than one entity.

--- Code/include/nu.hpp
+++ Code/include/nu.hpp
@@ -10,7 +10,7 @@
 #define nu_hpp
 
 // INCLUDES:
-  #include "mesh.hpp"                                                                               // Neutrino's mesh context declarations.
+  #include "algebraic_topology.hpp"                                                                 // Neutrino's mesh context declarations.
   #include "opengl.hpp"                                                                             // Neutrino's OpenGL context declarations.
   #include "opencl.hpp"                                                                             // Neutrino's OpenCL context declarations.
 
