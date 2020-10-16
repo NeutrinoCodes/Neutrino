5a2168ae6fdb1ae05a1c80a18b51b94d98262b53
ezorzin <erik@zorzin.com>
2020-10-16 19:40:29 +0200

    Debugging shader build.

--- Code/include/shader.hpp
+++ Code/include/shader.hpp
@@ -26,7 +26,7 @@
 class shader                                                                                        /// @brief **OpenGL shader.**
 {
 private:
-  neutrino*                baseline;                                                                ///< @brief **Neutrino baseline.**
+  neutrino*  baseline;                                                                              ///< @brief **Neutrino baseline.**
 
   /// @brief    **OpenGL shader compilation.**
   /// @details  It compiles an OpenGL shader.
@@ -36,19 +36,8 @@ private:
                  );
 
 public:
-  std::string              shader_home;                                                             ///< @brief **OpenGL shader home directory.**
-  std::string              vertex_file_name;                                                        ///< @brief **OpenGL vertex shader source file name.**
-  std::string              geometry_file_name;                                                      ///< @brief **OpenGL geometry shader source file name.**
-  std::string              geometry_file_name_2;                                                    ///< @brief **OpenGL geometry shader source file name.**
-  std::string              fragment_file_name;                                                      ///< @brief **OpenGL fragment shader source file name.**
-  std::vector<std::string> vertex_source;                                                           ///< @brief **OpenGL vertex shader source file.**
-  std::vector<std::string> geometry_source;                                                         ///< @brief **OpenGL geometry shader source file.**
-  std::vector<std::string> fragment_source;                                                         ///< @brief **OpenGL vertex shader source file.**
-  std::vector<GLuint>      vertex;                                                                  ///< @brief **OpenGL vertex shader id.**
-  std::vector<GLuint>      geometry;                                                                ///< @brief **OpenGL geometry shader id.**
-  std::vector<GLuint>      fragment;                                                                ///< @brief **OpenGL fragment shader id.**
-  GLuint                   program;                                                                 ///< @brief **OpenGL program.**
-  GLsizeiptr               size;                                                                    ///< @brief **OpenGL shader argument size.**
+  GLuint     program;                                                                               ///< @brief **OpenGL program.**
+  GLsizeiptr size;                                                                                  ///< @brief **OpenGL shader argument size.**
 
   /// @brief **Class constructor.**
   /// @details It does nothing.
@@ -57,83 +46,78 @@ public:
   /// @brief **Class initializer.**
   /// @details It compiles the OpenGL vertex, geometry and fragment shader into an OpenGL program.
   void init (
-             neutrino*   loc_baseline,                                                              ///< Neutrino baseline.
-             std::string loc_shader_home,                                                           ///< Shader home directory.
-             std::string loc_vertex_file_name,                                                      ///< Vertex shader file name.
-             std::string loc_geometry_file_name,                                                    ///< Geometry shader file name.
-             std::string loc_geometry_file_name_2,                                                  ///< Geometry shader file name.
-             std::string loc_fragment_file_name                                                     ///< Fragment shader file name.
+             neutrino* loc_baseline                                                                 ///< Neutrino baseline.
             );
 
   /// @brief **Shader source adder function.**
   /// @details Loads an OpenGL shader source from its corresponding source file.
-  GLuint addsource (
-                    std::string loc_shader_filename,                                                ///< GLSL shader file name.
-                    shader_type loc_shader_type                                                     ///< GLSL shader type.
-                   );
+  void addsource (
+                  std::string loc_shader_filename,                                                  ///< GLSL shader file name.
+                  shader_type loc_shader_type                                                       ///< GLSL shader type.
+                 );
 
   /// @brief    **OpenGL shader builder.**
   /// @details  It builds an OpenGL shader.
-  void   build ();
+  void build ();
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////// setarg "functions" //////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////////////////
   /// @brief **OpenGL shader argument setter function.**
   /// @details Sets an argument in the OpenGL shader.
-  void   setarg (
-                 nu_int* loc_data,                                                                  ///< Data object.
-                 GLuint  loc_layout_index                                                           ///< Data layout index.
-                );
+  void setarg (
+               nu_int* loc_data,                                                                    ///< Data object.
+               GLuint  loc_layout_index                                                             ///< Data layout index.
+              );
 
   /// @overload setarg(nu_int2* loc_data, GLuint loc_layout_index)
   /// @details Sets an argument in the OpenGL shader.
-  void   setarg (
-                 nu_int2* loc_data,                                                                 ///< Data object.
-                 GLuint   loc_layout_index                                                          ///< Data layout index.
-                );
+  void setarg (
+               nu_int2* loc_data,                                                                   ///< Data object.
+               GLuint   loc_layout_index                                                            ///< Data layout index.
+              );
 
   /// @overload setarg(nu_int3* loc_data, GLuint loc_layout_index)
   /// @details Sets an argument in the OpenGL shader.
-  void   setarg (
-                 nu_int3* loc_data,                                                                 ///< Data object.
-                 GLuint   loc_layout_index                                                          ///< Data layout index.
-                );
+  void setarg (
+               nu_int3* loc_data,                                                                   ///< Data object.
+               GLuint   loc_layout_index                                                            ///< Data layout index.
+              );
 
   /// @overload setarg(nu_int4* loc_data, GLuint loc_layout_index)
   /// @details Sets an argument in the OpenGL shader.
-  void   setarg (
-                 nu_int4* loc_data,                                                                 ///< Data object.
-                 GLuint   loc_layout_index                                                          ///< Data layout index.
-                );
+  void setarg (
+               nu_int4* loc_data,                                                                   ///< Data object.
+               GLuint   loc_layout_index                                                            ///< Data layout index.
+              );
 
   /// @overload setarg(nu_float* loc_data, GLuint loc_layout_index)
   /// @details Sets an argument in the OpenGL shader.
-  void   setarg (
-                 nu_float* loc_data,                                                                ///< Data object.
-                 GLuint    loc_layout_index                                                         ///< Data layout index.
-                );
+  void setarg (
+               nu_float* loc_data,                                                                  ///< Data object.
+               GLuint    loc_layout_index                                                           ///< Data layout index.
+              );
 
   /// @overload setarg(nu_float2* loc_data, GLuint loc_layout_index)
   /// @details Sets an argument in the OpenGL shader.
-  void   setarg (
-                 nu_float2* loc_data,                                                               ///< Data object.
-                 GLuint     loc_layout_index                                                        ///< Data layout index.
-                );
+  void setarg (
+               nu_float2* loc_data,                                                                 ///< Data object.
+               GLuint     loc_layout_index                                                          ///< Data layout index.
+              );
 
   /// @overload setarg(nu_float3* loc_data, GLuint loc_layout_index)
   /// @details Sets an argument in the OpenGL shader.
-  void   setarg (
-                 nu_float3* loc_data,                                                               ///< Data object.
-                 GLuint     loc_layout_index                                                        ///< Data layout index.
-                );
+  void setarg (
+               nu_float3* loc_data,                                                                 ///< Data object.
+               GLuint     loc_layout_index                                                          ///< Data layout index.
+              );
 
   /// @overload setarg(nu_float4* loc_data, GLuint loc_layout_index)
   /// @details Sets an argument in the OpenGL shader.
-  void   setarg (
-                 nu_float4* loc_data,                                                               ///< Data object.
-                 GLuint     loc_layout_index                                                        ///< Data layout index.
-                );
+  void setarg (
+               nu_float4* loc_data,                                                                 ///< Data object.
+               GLuint     loc_layout_index                                                          ///< Data layout index.
+              );
 
   /// @brief **Class destructor.**
   /// @details It does nothing.
